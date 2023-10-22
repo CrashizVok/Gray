851b6b5847da972db6bc114cd30e01a5c68fd5b7
CrashizVok <146128367+CrashizVok@users.noreply.github.com>
2023-10-23 00:17:36 +0200

    Update Gray.py

--- Gray.py
+++ Gray.py
@@ -22,19 +22,35 @@ def Banner():
 Banner()    
 x = int(input("Enter the attack number: "))
 if x == 1:
-    nmap_output = subprocess.check_output(['sudo', 'nmap', '-sn', '192.168.0.0/24'])
-    nmap_output = nmap_output.decode('utf-8')
-    pattern = re.compile(r'Host (\d+\.\d+\.\d+\.\d+) .*?([0-9A-Fa-f:]{17}) \((.*?)\)')
-    matches = pattern.findall(nmap_output)
-  
-    config_file_path = "hotspot.conf"
-  
-    for ip, mac, device_name in matches:
-        filename = f"{device_name}.txt"
-        with open(filename, "w") as file:
-            file.write(f"IP cím: {ip}")
+    def create_hotspot():
     subprocess.run(['sudo', 'systemctl', 'unmask', 'hostapd'])
     subprocess.run(['sudo', 'systemctl', 'start', 'hostapd'])
+
+    def track_connected_devices():
+        connected_devices = set() 
+    
+        while True:
+            nmap_output = subprocess.check_output(['sudo', 'nmap', '-sn', '192.168.0.0/24'])
+            nmap_output = nmap_output.decode('utf-8')
+            pattern = re.compile(r'Host (\d+\.\d+\.\d+\.\d+) .*?([0-9A-Fa-f:]{17}) \((.*?)\)')
+            matches = pattern.findall(nmap_output)
+    
+            for ip, mac, device_name in matches:
+                if mac not in connected_devices:
+                    print(f"Célpont bemérve: {device_name} ({ip})")
+                    connected_devices.add(mac)
+            
+            disconnected_devices = connected_devices.copy()
+            for ip, mac, _ in matches:
+                if mac in disconnected_devices:
+                    disconnected_devices.remove(mac)
+            
+            for mac in disconnected_devices:
+                print(f"{mac} lecsatlakozott")
+            time.sleep(60) 
+    create_hotspot()
+    track_connected_devices()
+    #tenksz jú
 if x == 2:
     print("Comming soon ")
 else:
