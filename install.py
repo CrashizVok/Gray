5587e73037e9359715d3f092576d2b62025d5bed
CrashizVok <146128367+CrashizVok@users.noreply.github.com>
2023-10-22 22:51:55 +0200

    install.py

---
+++ install.py
@@ -0,0 +1,15 @@
+config = """\
+interface=wlan0
+driver=nl80211
+ssid=YourHotspotName
+hw_mode=g
+channel=6
+wpa=2
+wpa_passphrase=YourPassword
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+"""
+
+with open("/etc/hostapd/hotspot.conf", "w") as config_file:
+    config_file.write(config)
