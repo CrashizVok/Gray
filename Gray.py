b661020b5d93cafe97f27453f9eb5e113bdea59d
CrashizVok <146128367+CrashizVok@users.noreply.github.com>
2023-10-23 00:23:36 +0200

    Update Gray.py

--- Gray.py
+++ Gray.py
@@ -23,11 +23,30 @@ Banner()
 x = int(input("Enter the attack number: "))
 if x == 1:
     def create_hotspot():
+        hotspot_config = """
+        interface=wlan0
+        driver=nl80211
+        ssid=YourHotspotName
+        hw_mode=g
+        channel=6
+        wmm_enabled=0
+        macaddr_acl=0
+        auth_algs=1
+        ignore_broadcast_ssid=0
+        wpa=2
+        wpa_passphrase=YourPassword
+        wpa_key_mgmt=WPA-PSK
+        wpa_pairwise=TKIP
+        rsn_pairwise=CCMP
+        """
+    with open("/etc/hostapd/hotspot.conf", "w") as config_file:
+        config_file.write(hotspot_config)
+
     subprocess.run(['sudo', 'systemctl', 'unmask', 'hostapd'])
     subprocess.run(['sudo', 'systemctl', 'start', 'hostapd'])
 
     def track_connected_devices():
-        connected_devices = set() 
+        connected_devices = set()  # Az aktuálisan csatlakoztatott eszközök nyomon követése
     
         while True:
             nmap_output = subprocess.check_output(['sudo', 'nmap', '-sn', '192.168.0.0/24'])
@@ -40,14 +59,16 @@ if x == 1:
                     print(f"Célpont bemérve: {device_name} ({ip})")
                     connected_devices.add(mac)
             
+            # Ellenőrizzük, hogy az aktuálisan nyomon követett eszközök továbbra is csatlakoztatva vannak
             disconnected_devices = connected_devices.copy()
             for ip, mac, _ in matches:
                 if mac in disconnected_devices:
-                    disconnected_devices.remove(mac)
+                    disconnected_devices remove(mac)
             
             for mac in disconnected_devices:
                 print(f"{mac} lecsatlakozott")
-            time.sleep(60) 
+    
+            time.sleep(60)
     create_hotspot()
     track_connected_devices()
     #tenksz jú
