f76fa8a859aed33373a7239aee72c4bcffe4438b
CrashizVok <146128367+CrashizVok@users.noreply.github.com>
2023-10-22 22:54:07 +0200

    WifiHotspotAttack.py

---
+++ WifiHotspotAttack.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.run(['sudo', 'hostapd', '/etc/hostapd/hotspot.conf'])
+arp_scan_output = subprocess.check_output(['sudo', 'arp-scan', '--localnet'])
+nmap_output = subprocess.check_output(['sudo', 'nmap', '-sn', '192.168.1.0/24'])
